ec54537e46b56f47e925159ecc509418829bbdb2
Christian Johannesson <christian.johannesson@drakryggen.com>
2021-01-18 10:20:01 +0100

    heroku commit

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const express = require("express");
 app = express();
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
+const path = require("path");
 
 app.use(cookieParser());
 app.use(express.json());
 
 //connect to databas
 mongoose.connect(
-  "mongodb://localhost:27017/loppis-db",
+  process.env.MONGODB_URI || "mongodb://localhost:27017/loppis-db",
   { useNewUrlParser: true, useUnifiedTopology: true },
   () => {
     console.log("connected to db");
@@ -19,7 +20,15 @@ mongoose.connect(
 const userRouter = require("./routes/User");
 app.use("/users", userRouter);
 
-const PORT = 5000;
+//set up static folder for rendering react app
+app.use(express.static(path.join(__dirname, "frontend/build")));
+
+//Handle any requests that doesn't target root url
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/frontend/build/index.html"));
+});
+
+const PORT = process.env.PORT || 5000;
 
 //listening for request on specified port
 app.listen(PORT, () => {
